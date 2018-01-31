05550e192025362bf7275c64232a3321371c25b9
ekalaja <eero.kalaja@helsinki.fi>
2018-01-31 15:13:21 +0100

    4.11 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  console.log(request.body)
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  console.log('request: ', request.body)
+  if (request.body.title === undefined || request.body.url === undefined) {
+    return response.status(400).json({ error: 'missing fields' })
+  }
+  try {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes === undefined ? 0: body.likes
+    }) /*
+    const blog = new Blog(request.body)*/
+    await blog.save()
+    response.json(blog)
+  }  catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong...' })
+  }
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -38,6 +38,29 @@ const initialBlogs = [
   }
 ]
 
+const additionalBlog = new Blog ({
+  title: 'Additional Blog one',
+  author: 'T. Author',
+  url: 'http://www.authorauthor.com',
+  likes: 2,
+  __v: 0
+})
+
+const noLikesFieldBlog = new Blog ({
+  title: 'Additional Blog Two',
+  author: 'Despised Writer',
+  url: 'http://www.nolikesfield.com',
+  __v: 0
+})
+
+const noTitleAndUrlBlog = new Blog ({
+  author: 'Titless Writer',
+  likes: 2
+})
+
+
+
+
 beforeAll(async () => {
   await Blog.remove({})
 
@@ -64,8 +87,6 @@ test('blogs are returned as json', async () => {
 test('there is 3 blogs', async () => {
   const response = await api
     .get('/api/blogs')
-  console.log('response: ', response.body)
-  console.log('response length', response.body.length)
   expect(response.body.length).toBe(4)
 })
 
@@ -75,6 +96,40 @@ test('the first blog is about HTTP methods', async () => {
   var firstBlog = response.body[0]
   expect(firstBlog.author).toBe('Maija Malli')
 })
+describe('Adding a new blog', async () => {
+  test('new blog with valid data is added to the database', async () => {
+    const originalBlogs = await Blog.find({})
+
+    await api
+      .post('/api/blogs')
+      .send(additionalBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAfter = await Blog.find({})
+    expect(blogsAfter.length).toBe(originalBlogs.length + 1)
+  })
+  test('blog without likes gets value 0 on likes', async () => {
+    const response = await api
+      .post('/api/blogs')
+      .send(noLikesFieldBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body.likes).toBe(0)
+    const blogsAfter = await Blog.find({})
+    const likes = blogsAfter.map(blog => blog.likes)
+    expect(likes).toEqual([7,7,5,12,2,0])
+
+  })
+  test('blog without title and url gets response bad request', async () => {
+    await api
+      .post('/api/blogs')
+      .send(noTitleAndUrlBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
 
 afterAll(() => {
   server.close()
