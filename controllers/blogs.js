33083d047ed3bde57cc8453cf2a39956ece82e51
ekalaja <eero.kalaja@helsinki.fi>
2018-02-06 09:09:59 +0100

    blogilla user ja userilla blogeja

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,29 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  response.json(blogs)
+    .populate('user', { id: 1, username: 1, name: 1 })
+  response.json(blogs.map(Blog.format))
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  console.log('request: ', request.body)
+  const user = await User.findById(body.userId)
   if (request.body.title === undefined || request.body.url === undefined) {
     return response.status(400).json({ error: 'missing fields' })
   }
   try {
-    if (body.likes === undefined) {
-      const blog = new Blog(body)
-      blog.likes = 0
-      await blog.save()
-      response.json(blog)
-    } else {
-      const blog = new Blog(body)
-      await blog.save()
-      response.json(blog)
-    }
+    const blog = new Blog(body)
+    blog.likes = body.likes === undefined ? 0 : body.likes
+    blog.user = user._id
+    await blog.save()
+    response.json(blog)
+    user.blogs = user.blogs.concat(blog._id)
+    await user.save()
   }  catch (exception) {
     console.log(exception)
     response.status(500).json({ error: 'something went wrong...' })
